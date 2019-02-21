391f630b567191a8fb96fce6b8b9ab481fe52bb4
Liubov Lonishin <43217334+Lonishin@users.noreply.github.com>
2019-02-21 21:44:54 +0300

    Update and rename Task1.1 to Family.R

--- Family.R
+++ Family.R
@@ -1,14 +1,15 @@
 library(dplyr)
 library(visNetwork)
 
-
-nodes <- data.frame(id = 1:4, group = c( "Mom", "Brother", "Dad", "Me"))
-edges <- data.frame(from = c(2,3,4,1), to = c(3,1,2,4))
+nodes <- data.frame(id = 1:4, group = c( "Mom", "Me","Dad", "Brother"))
+edges <- data.frame(from = c(1,2,3,4), to = c(2,3,4,1))
 
 visNetwork(nodes, edges, width = "100%") %>%
   visGroups(groupname = "Mom", shape = "icon", icon = list(code = "f182", size = 75)) %>%
   visGroups(groupname = "Brother", shape = "icon", icon = list(code = "f007", color = "red")) %>%
   visGroups(groupname = "Dad", shape = "icon", icon = list(code = "f183", color = "red", size = 75)) %>%
-  visGroups(groupname = "Me", shape = "icon", icon = list(code = "f007", color = "blue")) %>%
+  visGroups(groupname = "Me", shape = "icon", icon = list(code = "f007", color = "purple")) %>%
   visLegend() %>%
+  visEdges(length = 100,smooth = FALSE) %>%
+  visLayout(randomSeed = 11)  %>%
   addFontAwesome()
